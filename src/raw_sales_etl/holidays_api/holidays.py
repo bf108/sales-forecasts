c2d5fa083bb05eb4fd1c130e94cf981635c8ef1b
Ben Farrell <ben.farrell@iqvia.com>
2023-09-10 08:40:34 +0100

    Create url for holidays endpoint

--- src/raw_sales_etl/holidays_api/holidays.py
+++ src/raw_sales_etl/holidays_api/holidays.py
@@ -2,6 +2,8 @@ from datetime import date
 from datetime import datetime
 
 import pandas as pd
+from holidays_config import CALENDAR_META  # pylint: disable=import-error
+from holidays_config import CALENDAR_URL  # pylint: disable=import-error
 
 
 def create_calendar_df(start_dt: date, end_dt: date) -> pd.DataFrame:
@@ -18,3 +20,10 @@ def convert_ms_unix_timestamp_to_datatime(unix_timestamp: str) -> datetime:
     return datetime.strptime(
         datetime.utcfromtimestamp(ts_ / 1e3).strftime("%Y-%m-%d"), "%Y-%m-%d"
     )
+
+
+def create_url(country: str, year: int) -> str:
+    if country.lower() in CALENDAR_META:
+        meta = CALENDAR_META[country.lower()]
+        return f"{CALENDAR_URL}/{country}/{year}?hol={meta}"
+    return f"{CALENDAR_URL}/{country}/{year}"
