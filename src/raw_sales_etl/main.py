16e2ebe508d544d1c279660cd8ad52e218c9af4e
Ben Farrell <ben.farrell@iqvia.com>
2023-10-05 16:46:33 +0100

    Final script to generate and store holiday scaling factors

--- src/raw_sales_etl/main.py
+++ src/raw_sales_etl/main.py
@@ -1,32 +1,34 @@
 from datetime import date
 from pathlib import Path
+
 import pandas as pd
-from sales_data_etl.etl import (
-    read_meta_from_disk,
-    read_sales_from_disk,
-    create_calendar_df,
-    etl_pipeline,
-)
 from holidays_api.holidays import create_combined_holidays_df
+from sales_data_etl.etl import create_calendar_df
+from sales_data_etl.etl import etl_pipeline
+from sales_data_etl.etl import read_meta_from_disk
+from sales_data_etl.etl import read_sales_from_disk
 
 
 def main() -> None:
     sd = date(2021, 1, 1)
     ed = date(2024, 9, 28)
     df_cal = create_calendar_df(sd, ed)
-    #Update to path in AWS
-    df = pd.read_csv('../raw_sales_etl/data/hourly_sales_per_branch.csv')
-    df['ds'] = pd.to_datetime(df['ds'])
-    df['ds'] = df['ds'].dt.date
-    df_s = (df.groupby(by=['unique_id','ds'])['y'].sum().reset_index().sort_values(by=['ds'], ascending=True)
-            .reset_index(drop=True))
-    #Update to path in AWS
-    # df_s = read_sales_from_disk(
-    #     Path("../raw_sales_etl/data/daily_sales_data_all_branches.csv")
-    # )
-    #Update to path in AWS
+    # Update to path in S3
+    df = pd.read_csv("../raw_sales_etl/data/hourly_sales_per_branch.csv")
+    df["ds"] = pd.to_datetime(df["ds"])
+    df["ds"] = df["ds"].dt.date
+    df_s = (
+        df.groupby(by=["unique_id", "ds"])["y"]
+        .sum()
+        .reset_index()
+        .sort_values(by=["ds"], ascending=True)
+        .reset_index(drop=True)
+    )
+    # Update to path in S3
     df_m = read_meta_from_disk(Path("../raw_sales_etl/data/branches.csv"))
-    df_hols = create_combined_holidays_df(countries=["uk", "ireland"], years=[2021, 2022, 2023])
+    df_hols = create_combined_holidays_df(
+        countries=["uk", "ireland", "malta", "spain"], years=[2021, 2022, 2023]
+    )
     df_output = etl_pipeline(
         df_sales=df_s,
         df_meta=df_m,
@@ -35,9 +37,11 @@ def main() -> None:
         unique_id_sales="unique_id",
         unique_id_meta="_id",
         sales_col="y",
-        sales_adj_prc_th=0.05
+        sales_adj_prc_th=0.05,
+        holiday_scaling_factor_contribution=0.5,
     )
-    print(df_output.shape)
+    df_output.to_csv("AWS/S3/Location/holiday_scaling_factors_2021_2023.csv")
+
 
 if __name__ == "__main__":
     main()
