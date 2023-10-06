392da8496f1dca32b4402bc4c19254caf96d5c0c
Ben Farrell <ben.farrell@iqvia.com>
2023-10-06 11:16:54 +0100

    Updated main func to have file path for S3

--- src/raw_sales_etl/main.py
+++ src/raw_sales_etl/main.py
@@ -1,5 +1,7 @@
 from datetime import date
 from pathlib import Path
+import logging
+import os
 
 import pandas as pd
 from holidays_api.holidays import create_combined_holidays_df
@@ -8,13 +10,28 @@ from sales_data_etl.etl import etl_pipeline
 from sales_data_etl.etl import read_meta_from_disk
 from sales_data_etl.etl import read_sales_from_disk
 
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler())
+
+base_dir = "/opt/ml/processing"
+
 
 def main() -> None:
     sd = date(2021, 1, 1)
-    ed = date(2024, 9, 28)
+    ed = date(2023, 12, 31)
     df_cal = create_calendar_df(sd, ed)
     # Update to path in S3
-    df = pd.read_csv("../raw_sales_etl/data/hourly_sales_per_branch.csv")
+    path_hourly_sales_data = os.path.join(base_dir, "input", "hourly_sales_per_branch")
+    filepath_hourly_sales_data = os.path.join(
+        path_hourly_sales_data, os.listdir(path_hourly_sales_data)[0]
+    )
+
+    logger.info(
+        f"Loading forecasts data from path '{filepath_hourly_sales_data}' into DataFrame..."
+    )
+
+    df = pd.read_csv(filepath_hourly_sales_data)
     df["ds"] = pd.to_datetime(df["ds"])
     df["ds"] = df["ds"].dt.date
     df_s = (
@@ -25,7 +42,11 @@ def main() -> None:
         .reset_index(drop=True)
     )
     # Update to path in S3
-    df_m = read_meta_from_disk(Path("../raw_sales_etl/data/branches.csv"))
+    path_meta_data = os.path.join(base_dir, "input", "hourly_sales_per_branch")
+    filepath_meta_data = os.path.join(path_meta_data, os.listdir(path_meta_data)[0])
+
+    logger.info(f"Loading meta data from path '{filepath_meta_data}' into DataFrame...")
+    df_m = read_meta_from_disk(Path(filepath_meta_data))
     df_hols = create_combined_holidays_df(
         countries=["uk", "ireland", "malta", "spain"], years=[2021, 2022, 2023]
     )
@@ -38,9 +59,13 @@ def main() -> None:
         unique_id_meta="_id",
         sales_col="y",
         sales_adj_prc_th=0.05,
-        holiday_scaling_factor_contribution=0.5,
+        holiday_scaling_factor_contribution=1,
     )
-    df_output.to_csv("AWS/S3/Location/holiday_scaling_factors_2021_2023.csv")
+    # df_output.to_csv("AWS/S3/Location/holiday_scaling_factors_2021_2023.csv")
+    output_filename = f"holiday_scaling_factors_2021_2023_{date.today()}.csv"
+    logger.info(f"Storing forecasts to {output_filename}...")
+
+    df_output.to_csv(f"{base_dir}/output/{output_filename}")
 
 
 if __name__ == "__main__":
